3faec54febc9cab6a3eae055972c7d8b774458a3
Taisei Nishiumi <westelh@gmail.com>
2024-07-11 13:58:16 +0900

    Remove interface AbstractClientFactory

--- src/main/kotlin/dev/westelh/oci/billing/exporter/api/AbstractClientFactory.kt
+++
@@ -1,7 +0,0 @@
-package dev.westelh.oci.billing.exporter.api
-
-import com.oracle.bmc.objectstorage.ObjectStorageClient
-
-interface AbstractClientFactory {
-    fun createClient(): ObjectStorageClient
-}
--- src/main/kotlin/dev/westelh/oci/billing/exporter/api/ClientFactory.kt
+++ src/main/kotlin/dev/westelh/oci/billing/exporter/api/ClientFactory.kt
@@ -4,14 +4,14 @@ import com.oracle.bmc.Region
 import com.oracle.bmc.auth.*
 import com.oracle.bmc.objectstorage.ObjectStorageClient
 
-class ClientFactory(private val region: Region, private val auth: AbstractAuthenticationDetailsProvider) : AbstractClientFactory {
+class ClientFactory(private val region: Region, private val auth: AbstractAuthenticationDetailsProvider) {
     constructor(auth: ConfigFileAuthenticationDetailsProvider): this(auth.region, auth)
     constructor(auth: InstancePrincipalsAuthenticationDetailsProvider): this(auth.region, auth)
     constructor(auth: ResourcePrincipalAuthenticationDetailsProvider): this(auth.region, auth)
     constructor(auth: SessionTokenAuthenticationDetailsProvider): this(auth.region, auth)
     constructor(auth: SimpleAuthenticationDetailsProvider): this(auth.region, auth)
 
-    override fun createClient(): ObjectStorageClient = ObjectStorageClient.builder()
+    fun createClient(): ObjectStorageClient = ObjectStorageClient.builder()
             .region(region)
             .clientConfigurator(GlobalClientConfigurator.configurator)
             .build(auth)
