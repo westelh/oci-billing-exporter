1cd2fd2c78908242a7211c59969def93e15f92c9
Taisei Nishiumi <westelh@gmail.com>
2024-07-28 19:56:30 +0900

    Add new interface
    Add new interface ObjectStorageFactory.getObjectStorage() and its implementations

--- src/main/kotlin/dev/westelh/oci/billing/exporter/client/CachedObjectStorage.kt
+++ src/main/kotlin/dev/westelh/oci/billing/exporter/client/CachedObjectStorage.kt
@@ -1,12 +1,14 @@
 package dev.westelh.oci.billing.exporter.client
 
 import com.oracle.bmc.auth.AuthenticationDetailsProvider
+import com.oracle.bmc.objectstorage.ObjectStorage
 import com.oracle.bmc.objectstorage.ObjectStorageAsync
 import com.oracle.bmc.objectstorage.ObjectStorageAsyncClient
+import com.oracle.bmc.objectstorage.ObjectStorageClient
 
 class CachedObjectStorage(adp: AuthenticationDetailsProvider) : ObjectStorageFactory {
-    private val os: ObjectStorageAsyncClient = ObjectStorageAsyncClient.builder().build(adp)
-    override fun getObjectStorageAsync(): ObjectStorageAsync {
-        return os
-    }
+    private val os = ObjectStorageClient.builder().build(adp)
+    private val osAsync: ObjectStorageAsyncClient = ObjectStorageAsyncClient.builder().build(adp)
+    override fun getObjectStorage(): ObjectStorage = os
+    override fun getObjectStorageAsync(): ObjectStorageAsync = osAsync
 }
--- src/main/kotlin/dev/westelh/oci/billing/exporter/client/ObjectStorageFactory.kt
+++ src/main/kotlin/dev/westelh/oci/billing/exporter/client/ObjectStorageFactory.kt
@@ -1,7 +1,9 @@
 package dev.westelh.oci.billing.exporter.client
 
+import com.oracle.bmc.objectstorage.ObjectStorage
 import com.oracle.bmc.objectstorage.ObjectStorageAsync
 
 interface ObjectStorageFactory {
+    fun getObjectStorage(): ObjectStorage
     fun getObjectStorageAsync(): ObjectStorageAsync
 }
