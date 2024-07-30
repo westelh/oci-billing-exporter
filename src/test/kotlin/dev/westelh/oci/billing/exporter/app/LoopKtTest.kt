13afad87a40cfd2e7fcd9733deba06e8d6e9e38d
Taisei Nishiumi <westelh@gmail.com>
2024-07-30 09:28:03 +0900

    Add test

--- src/test/kotlin/dev/westelh/oci/billing/exporter/app/LoopKtTest.kt
+++ src/test/kotlin/dev/westelh/oci/billing/exporter/app/LoopKtTest.kt
@@ -1,19 +1,27 @@
 package dev.westelh.oci.billing.exporter.app
 
+import io.kotest.assertions.throwables.shouldNotThrowAny
 import io.kotest.matchers.concurrent.suspension.shouldCompleteWithin
-import kotlinx.coroutines.cancelAndJoin
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import java.util.concurrent.TimeUnit
 import kotlin.test.Test
 import kotlin.time.Duration
 
 class LoopKtTest {
     @Test
-    fun cancellable() {
+    fun jobIsCancellable() {
         runBlocking {
             shouldCompleteWithin(100, TimeUnit.MILLISECONDS) {
                 launch { loop(Duration.INFINITE) { } }.cancelAndJoin()
+            }
+        }
+    }
+
+    @Test
+    fun cancellationExceptionDoesNotHaltTheCode() {
+        runBlocking {
+            shouldNotThrowAny {
+                launch { loop(Duration.ZERO) { throw CancellationException() } }
             }
         }
     }
