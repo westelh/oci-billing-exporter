16c31462f9b55bbb0c3b9d0c7ea3a1531d5132db
Taisei Nishiumi <westelh@gmail.com>
2024-08-06 00:33:45 +0900

    Update authentication flow

--- src/main/kotlin/dev/westelh/obe/Run.kt
+++ src/main/kotlin/dev/westelh/obe/Run.kt
@@ -6,17 +6,20 @@ import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.types.file
 import com.google.common.flogger.FluentLogger
+import com.oracle.bmc.auth.AuthenticationDetailsProvider
+import com.oracle.bmc.objectstorage.ObjectStorage
+import com.oracle.bmc.objectstorage.ObjectStorageAsync
+import com.oracle.bmc.objectstorage.ObjectStorageAsyncClient
+import com.oracle.bmc.objectstorage.ObjectStorageClient
 import com.oracle.bmc.objectstorage.requests.GetObjectRequest
+import com.oracle.bmc.objectstorage.requests.ListObjectsRequest
 import com.oracle.bmc.objectstorage.transfer.DownloadManager
 import dev.westelh.obe.client.billingBucketName
 import dev.westelh.obe.client.billingNamespace
-import dev.westelh.obe.client.objectstorage.CachedObjectStorage
-import dev.westelh.obe.client.objectstorage.SimpleRequestFactory
+import dev.westelh.obe.client.billingPrefixForCostReport
 import dev.westelh.obe.client.objectstorage.listAllObjects
 import dev.westelh.obe.client.objectstorage.suspendGetObject
-import dev.westelh.obe.config.Config
-import dev.westelh.obe.config.buildDownloadConfiguration
-import dev.westelh.obe.config.loadAuthConfig
+import dev.westelh.obe.config.*
 import dev.westelh.obe.core.CsvParser
 import io.prometheus.metrics.instrumentation.jvm.JvmMetrics
 import kotlinx.coroutines.CancellationException
@@ -28,7 +31,10 @@ import kotlin.time.DurationUnit
 import kotlin.time.toKotlinDuration
 
 class Run : CliktCommand() {
-    private val configFile by option("--config").file(mustBeReadable = true, canBeDir = false).required()
+    private val config by option("--config").file(mustBeReadable = true, canBeDir = false).convert {
+        Config.fromYaml(it)
+    }.required()
+
     private val metrics = Metrics()
 
     init {
@@ -40,28 +46,26 @@ class Run : CliktCommand() {
     }
 
     override fun run() {
-        val os = ObjectStorageClient.builder().build(adp)
-        val osAsync = ObjectStorageAsyncClient.builder().build(adp)
-        val dlManager = DownloadManager(os, buildDownloadConfiguration(config.server.download))
-
         runBlocking {
-            val requestFactory = SimpleRequestFactory(config.targetTenantId)
 
             logger.atInfo().log("Starting main loop.")
             loop(config.server.delay.toKotlinDuration()) {
                 try {
 
+                    // To get SDK initialized eagerly, trying build them inside loop and try{}.
+                    val auth = getAuthentication()
+                    val objectStorage = buildObjectStorage(auth)
+                    val objectStorageAsync = buildObjectStorageAsync(auth)
+                    val downloadManager = buildDownloadManager(objectStorage)
+
                     logger.atFine().log("Getting the list of objects in the designated object storage bucket.")
-                    val all = listAllObjects(osAsync, requestFactory.buildListCostReportsRequest())
+                    val all = listAllObjects(objectStorageAsync, buildRequestListObjectOfCostReport())
 
                     if (all.isNotEmpty()) {
                         val latest = all.maxBy { it.name }.name
 
                         logger.atFine().log("Downloading the latest cost report.")
-                        val getRes = dlManager.suspendGetObject(
-                            GetObjectRequest.builder().billingNamespace().billingBucketName(config.targetTenantId)
-                                .objectName(latest).build()
-                        )
+                        val getRes = downloadManager.suspendGetObject(buildRequestGetObjectOfCostReport(latest))
 
                         logger.atFine().log("Parsing object content as cost report.")
                         val report = CsvParser().parse(GZIPInputStream(getRes.inputStream))
@@ -80,9 +84,31 @@ class Run : CliktCommand() {
             }
         }
     }
+
+    private fun getAuthentication(): AuthenticationDetailsProvider =
+        config.auth.anythingAvailable() ?: throw RuntimeException("All of the auth methods provided was unavailable.")
+
+    private fun buildObjectStorage(adp: AuthenticationDetailsProvider): ObjectStorage = ObjectStorageClient.builder().build(adp)
+
+    private fun buildObjectStorageAsync(adp: AuthenticationDetailsProvider): ObjectStorageAsync = ObjectStorageAsyncClient.builder().build(adp)
+
+    private fun buildDownloadManager(objectStorage: ObjectStorage): DownloadManager =
+        DownloadManager(objectStorage, buildDownloadConfiguration(config.server.download))
+
+    private fun buildRequestListObjectOfCostReport(): ListObjectsRequest = ListObjectsRequest.builder()
+        .billingNamespace()
+        .billingBucketName(config.targetTenantId)
+        .billingPrefixForCostReport()
+        .build()
+
+    private fun buildRequestGetObjectOfCostReport(name: String): GetObjectRequest = GetObjectRequest.builder()
+        .billingNamespace()
+        .billingBucketName(config.targetTenantId)
+        .objectName(name)
+        .build()
 }
 
-suspend fun<R> loop(delay: Duration, job: suspend () -> R) {
+suspend fun <R> loop(delay: Duration, job: suspend () -> R) {
     while (true) {
         job()
         delay(delay)
--- src/main/kotlin/dev/westelh/obe/config/AuthConfig.kt
+++ src/main/kotlin/dev/westelh/obe/config/AuthConfig.kt
@@ -2,22 +2,9 @@ package dev.westelh.obe.config
 
 import com.oracle.bmc.auth.AuthenticationDetailsProvider
 
-fun loadAuthConfig(config: Config.AuthConfig): Result<AuthenticationDetailsProvider> = runCatching {
-    if (config.instancePrincipal != null) {
-//        logger.atInfo().log("Authenticating instance principal")
-        loadInstancePrincipalConfig(config.instancePrincipal)
-    }
-    if (config.resourcePrincipal != null) {
-//        logger.atInfo().log("Authenticating resource principal")
-        loadResourcePrincipalConfig(config.resourcePrincipal)
-    }
-//    logger.atInfo().log("Authenticating by oci config file")
-    loadFileConfig(config.config)
-}
-
-fun readAuthenticationDetailsProviderFrom(config: Config.AuthConfig): AuthenticationDetailsProvider? {
-    runCatching { loadInstancePrincipalConfig(config.instancePrincipal!!) }.onSuccess { return it as AuthenticationDetailsProvider }
-    runCatching { loadResourcePrincipalConfig(config.resourcePrincipal!!) }.onSuccess { return it as AuthenticationDetailsProvider }
-    runCatching { loadFileConfig(config.config) }.onSuccess { return it }
+fun Config.AuthConfig.anythingAvailable(): AuthenticationDetailsProvider? {
+    runCatching { loadInstancePrincipalConfig(instancePrincipal!!) }.onSuccess { return it as AuthenticationDetailsProvider }
+    runCatching { loadResourcePrincipalConfig(resourcePrincipal!!) }.onSuccess { return it as AuthenticationDetailsProvider }
+    runCatching { loadFileConfig(config) }.onSuccess { return it }
     return null
 }
